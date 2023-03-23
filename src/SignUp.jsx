2da64e031a276898977d51301ad0117f1d80adbf
eslam <eslammahmoud3454@gmail.com>
2023-03-23 12:40:40 +0200

    google signup button

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
 
 function SignUp() {
   return (
@@ -10,7 +12,7 @@ function SignUp() {
           <input id="FullName" placeholder="Full Name" />
         </label>
         <label htmlFor="Email">
-        Email Address
+          Email Address
           <input id="Email" placeholder="Email Address" />
         </label>
         <label htmlFor="Password">
@@ -18,6 +20,16 @@ function SignUp() {
           <input id="Password" placeholder="Password" />
         </label>
         <button>Sign Up</button>
+        <GoogleOAuthProvider clientId="588094948888-gn1t0o88alj2i1sf38p5grdgnfl04uuu.apps.googleusercontent.com">
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              console.log(credentialResponse);
+            }}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+          />
+        </GoogleOAuthProvider>
       </form>
     </div>
   );
