fdcdfbc20b549403eb2c3968ed9094d71afba4c5
Amrnash <amrnashaat8@gmail.com>
2023-04-14 10:46:19 +0200

    Populate doctor data inside doctorinfo component

--- src/components/DoctorInfo.jsx
+++ src/components/DoctorInfo.jsx
@@ -8,22 +8,26 @@ import {
   Button,
 } from "@mui/material";
 import profile from "../assets/doctor.jpg";
-export const DoctorInfo = () => {
+export const DoctorInfo = ({ doctor }) => {
+  const ratingSum = doctor.reviews.reduce((acc, review) => {
+    return acc + review.rating;
+  }, 0);
+  const rating = ratingSum / doctor.reviews.length;
   return (
     <Card>
       <CardContent>
         <Stack direction="column" alignItems="center">
           <Avatar src={profile} sx={{ width: 80, height: 80 }} />
           <Typography variant="h6" color="primary" sx={{ mt: 2 }}>
-            Dr. John Doe
+            {doctor.name}
           </Typography>
           <Typography variant="subtitle1" color="grey">
-            Oncologist
+            {doctor.specialization}
           </Typography>
           <Stack direction="row" sx={{ mt: 1 }} alignItems="center">
-            <Rating value={4.5} precision={0.5} sx={{ mr: 1 }} />
+            <Rating value={rating} precision={0.5} sx={{ mr: 1 }} />
             <Typography variant="body2" color="grey">
-              (130-Reviews)
+              ({doctor.reviews.length}-Reviews)
             </Typography>
           </Stack>
           <Typography variant="body1" color="primary" sx={{ mt: 2 }}>
