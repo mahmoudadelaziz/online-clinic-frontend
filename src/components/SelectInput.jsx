3b05517f9796d5c4c5b4bfaabadf0d2168e87a67
Amrnash <amrnashaat8@gmail.com>
2023-04-20 08:13:57 +0200

    Add search and select functionalities

--- src/components/SelectInput.jsx
+++ src/components/SelectInput.jsx
@@ -1,17 +1,12 @@
-import {
-  FormControl,
-  FormHelperText,
-  InputLabel,
-  MenuItem,
-  Select,
-} from "@mui/material";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import { useState } from "react";
 
-export const SelectInput = ({ options, label }) => {
-  const [option, setOption] = useState("");
-  const handleCitySelectChange = (event) => {
-    setOption(event.target.value);
-  };
+export const SelectInput = ({
+  options,
+  label,
+  selectedOption,
+  handleOptionSelect,
+}) => {
   return (
     <>
       <FormControl>
@@ -19,8 +14,8 @@ export const SelectInput = ({ options, label }) => {
         <Select
           labelId="city-select-label"
           label={label}
-          value={option}
-          onChange={handleCitySelectChange}
+          value={selectedOption}
+          onChange={(e) => handleOptionSelect(e)}
           variant="filled"
           sx={{ width: 200 }}
         >
--- src/utility/axios.js
+++ src/utility/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
+  timeout: 5000,
 });
 
 export default instance;
