d1aa77b37fe7cc6f6ab57a453368f95b693ebfce
mahmoudadelaziz <mahmoudaziz501@gmail.com>
2023-06-15 18:45:37 +0300

    Created a placeholder Doctor's Info Page using MUI grid

--- src/pages/DoctorProfile.jsx
+++ src/pages/DoctorProfile.jsx
@@ -1,3 +1,51 @@
-export const DoctorProfile = () => {
-  return <>Hello world</>;
+import { DoctorInfo, SelectInput } from "../components";
+import { Doctors } from "./Doctors";
+import profile from "../assets/doctor.jpg";
+import {
+  Card,
+  Divider,
+  CardContent,
+  Avatar,
+  Grid,
+  Stack,
+  Typography,
+  Rating,
+  Button,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+
+export const DoctorProfile = ({ doctor }) => {
+  return (
+    <Card>
+      <CardContent>
+        <Grid container spacing={0} my={2}>
+          <Grid item xs={2}>
+            <Avatar src={profile} sx={{ width: 200, height: 200 }} />
+          </Grid>
+          <Grid item xs={10}>
+            <Typography variant="h3" color="primary">
+              Doctor's Name
+            </Typography>
+            <Typography variant="subtitle1" color="grey">
+              Doctor's Specialty
+            </Typography>
+            <Typography variant="subtitle2" color="grey">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              ligula velit, iaculis semper sem quis, consectetur volutpat
+              tellus. Aliquam et nulla efficitur, laoreet mauris id, tincidunt
+              lectus. Cras sed eleifend orci. Nam ut est egestas, accumsan lorem
+              ut, dignissim nisi. Fusce egestas scelerisque vulputate.
+            </Typography>
+            <Stack my={2}>
+            <Rating value={4.5} precision={0.5} />
+            <Typography vatiant="subtitle2">
+              Overall rating from 23 visitors
+            </Typography>
+            <Link to={`api/reviews`}>Show all reviews</Link>
+            </Stack>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
 };
