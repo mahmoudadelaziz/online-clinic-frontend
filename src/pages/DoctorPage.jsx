4d08d8f4a2771b0bb67df884ea8f3aaa05872396
Amrnash <amrnashaat8@gmail.com>
2023-06-17 03:18:49 +0300

    Modify some styles in DoctorPage

--- src/pages/DoctorPage.jsx
+++ src/pages/DoctorPage.jsx
@@ -1,5 +1,3 @@
-import { DoctorInfo, SelectInput } from "../components";
-import { Doctors } from "./Doctors";
 import { useParams } from "react-router-dom";
 import profile from "../assets/doctor.jpg";
 import {
@@ -12,6 +10,7 @@ import {
   Typography,
   Rating,
   Button,
+  Container,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import { Review } from "./Review"; // STILL NEED TO PASS VARIABLES TO EACH REVIEW
@@ -22,85 +21,104 @@ const AvailableSlots = ["slot1", "slot2", "slot3", "slot4", "slot5"];
 export const DoctorPage = () => {
   const { name } = useParams();
   return (
-    <Card>
-      <CardContent>
-        <Grid container spacing={0} my={2} padding={8}>
-          <Grid item xs={2}>
-            <Avatar src={profile} sx={{ width: 200, height: 200 }} />
-          </Grid>
-          <Grid item xs={10}>
-            <Typography variant="h3" color="primary">
-              Doctor's Name
-            </Typography>
-            <Typography variant="body2" color="grey">
-              Doctor's Specialty
-            </Typography>
-            <Typography variant="body2" color="grey">
-              Location
-            </Typography>
-            <Typography variant="body1" color="grey">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-              ligula velit, iaculis semper sem quis, consectetur volutpat
-              tellus. Aliquam et nulla efficitur, laoreet mauris id, tincidunt
-              lectus. Cras sed eleifend orci. Nam ut est egestas, accumsan lorem
-              ut, dignissim nisi. Fusce egestas scelerisque vulputate.
-            </Typography>
-            <Stack my={2}>
-              <Rating value={4.5} precision={0.5} />
-              <Typography vatiant="subtitle2">
-                Overall rating from 23 visitors
+    <Container sx={{ mt: 2 }}>
+      <Card>
+        <CardContent>
+          <Stack direction="row" justifyContent="space-between">
+            <Avatar src={profile} sx={{ width: 150, height: 150 }} />
+            <Stack flexBasis="2" sx={{ p: 2 }}>
+              <Typography variant="h4" color="primary">
+                Doctor's Name
               </Typography>
-              <Link to={`api/reviews`}>Show all reviews</Link>
+              <Typography variant="body2" color="grey">
+                Doctor's Specialty
+              </Typography>
+              <Typography variant="body2" color="grey" my={1}>
+                Location
+              </Typography>
+              <Typography variant="body1" color="grey">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                ligula velit, iaculis semper sem quis, consectetur volutpat
+                tellus. Aliquam et nulla efficitur, laoreet mauris id, tincidunt
+                lectus. Cras sed eleifend orci. Nam ut est egestas, accumsan
+                lorem ut, dignissim nisi. Fusce egestas scelerisque vulputate.
+              </Typography>
+              <Stack my={2}>
+                <Rating value={4.5} precision={0.5} />
+                <Typography variant="subtitle2" color="grey">
+                  Overall rating from 23 visitors
+                </Typography>
+              </Stack>
             </Stack>
+          </Stack>
+        </CardContent>
+      </Card>
 
-            <Stack my={12}>
-              <Typography variant="h4">Available Time Slots</Typography>
-              <Grid padding={2} container spacing={20} alignItems="center">
-                <Grid item xs>
-                  <Typography variant="h6">Today</Typography>
-                  <Stack spacing={0.5}>
-                    {AvailableSlots.map((slot) => {
-                      return <Button variant="outlined">{slot}</Button>;
-                    })}
-                  </Stack>
-                </Grid>
-                <Grid item xs>
-                  <Typography variant="h6">Tomorrow</Typography>
-                  <Stack spacing={0.5}>
-                    {AvailableSlots.map((slot) => {
-                      return <Button variant="outlined">{slot}</Button>;
-                    })}
-                  </Stack>
-                </Grid>
-                <Grid item xs>
-                  <Typography variant="h6">After 2 days</Typography>
-                  <Stack spacing={0.5}>
-                    {AvailableSlots.map((slot) => {
-                      return <Button variant="outlined">{slot}</Button>;
-                    })}
-                  </Stack>
-                </Grid>
-              </Grid>
-            </Stack>
-
-            <Stack>
-              <Typography variant="h4">Patients' Reviews</Typography>
-            </Stack>
-
-            {/* REVIEWS */}
-            <Stack
-              padding={2}
-              spacing={3}
-              divider={<Divider orientation="horizontal" flexItem />}
-            >
-              {/* One Review */}
-              <Review />
-              <Review />
-              <Review />
-            </Stack>
+      <Card sx={{ width: "100%", my: 2 }}>
+        <CardContent>
+          <Typography variant="h4" color="primary" textAlign="center" my={2}>
+            Available Time Slots
+          </Typography>
+          <Grid
+            padding={2}
+            container
+            spacing={20}
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Grid item xs>
+              <Typography variant="h6" textAlign="center">
+                Today
+              </Typography>
+              <Stack spacing={0.5}>
+                {AvailableSlots.map((slot) => {
+                  return <Button variant="outlined">{slot}</Button>;
+                })}
+              </Stack>
+            </Grid>
+            <Grid item xs>
+              <Typography variant="h6" textAlign="center">
+                Tomorrow
+              </Typography>
+              <Stack spacing={0.5}>
+                {AvailableSlots.map((slot) => {
+                  return <Button variant="outlined">{slot}</Button>;
+                })}
+              </Stack>
+            </Grid>
+            <Grid item xs>
+              <Typography variant="h6" textAlign="center">
+                After 2 days
+              </Typography>
+              <Stack spacing={0.5}>
+                {AvailableSlots.map((slot) => {
+                  return <Button variant="outlined">{slot}</Button>;
+                })}
+              </Stack>
+            </Grid>
           </Grid>
-        </Grid>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
+
+      <Card sx={{ p: 2 }}>
+        <Stack>
+          <Typography variant="h4" color="primary" textAlign="center" my={2}>
+            Patients' Reviews
+          </Typography>
+        </Stack>
+
+        {/* REVIEWS */}
+        <Stack
+          padding={2}
+          spacing={3}
+          divider={<Divider orientation="horizontal" flexItem />}
+        >
+          {/* One Review */}
+          <Review />
+          <Review />
+          <Review />
+        </Stack>
+      </Card>
+    </Container>
   );
 };
