cba880ae164f3578e74f2949d2c7ae00cb8d25b0
mahmoudadelaziz <mahmoudaziz501@gmail.com>
2023-06-20 01:30:08 +0300

    Checkpoint

--- src/components/TimeSlots.jsx
+++ src/components/TimeSlots.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
+import React, { useState } from 'react';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
-const DaySlots = ({ slotDuration = 20, reservedSlots = [] }) => {
+const DaySlots = ({ slotDuration = 30 }) => {
   const [selectedSlot, setSelectedSlot] = useState(null);
 
   const handleSlotClick = (slotIndex) => {
@@ -14,71 +14,60 @@ const DaySlots = ({ slotDuration = 20, reservedSlots = [] }) => {
     }
   };
 
-  const isReserved = (slotIndex) => reservedSlots.includes(slotIndex);
-
   const rows = [];
-  for (let i = 9; i < 17; i++) {
-    // 9am to 5pm
+  for (let i = 9; i < 17; i++) { // 9am to 5pm
     const row = [];
     for (let j = 0; j < 60 / slotDuration; j++) {
       const minute = j * slotDuration;
-      const time = `${i.toString().padStart(2, "0")}:${minute
-        .toString()
-        .padEnd(2, "0")}`;
+      const time = `${i.toString().padStart(2, '0')}:${minute.toString().padEnd(2, '0')}`;
       const index = i * 60 + minute;
-      const disabled = isReserved(index);
-      row.push({ time, index, disabled });
+      row.push({ time, index });
     }
     rows.push(row);
   }
 
   return (
-    <Box sx={{ height: "400px", overflowY: "auto" }}>
+    <Box sx={{ height: '200px', overflowY: 'auto' }}>
       <Grid container spacing={2}>
         {rows.map((row, i) => (
           <Grid item xs={12} key={i}>
             <Grid container spacing={2}>
-              {row.map((slot) => (
-                <Grid item key={slot.index}>
-                  <Button
-                    variant="outlined"
-                    sx={{
-                      width: `${slotDuration}px`,
-                      height: `${slotDuration}px`,
-                      border: "1px solid rgba(0, 0, 0, 0.12)",
-                      borderRadius: "4px",
-                      backgroundColor:
-                        selectedSlot === slot.index
-                          ? "green"
-                          : slot.disabled
-                          ? "rgba(0, 0, 0, 0.12)"
-                          : "white",
-                      color:
-                        selectedSlot === slot.index
-                          ? "white"
-                          : slot.disabled
-                          ? "rgba(0, 0, 0, 0.26)"
-                          : "inherit",
-                      cursor: slot.disabled ? "default" : "pointer",
-                      transition: "background-color 0.2s ease-in-out",
-                      "&:hover": {
-                        backgroundColor:
-                          selectedSlot === slot.index
-                            ? "green"
-                            : slot.disabled
-                            ? "rgba(0, 0, 0, 0.12)"
-                            : "rgba(0, 0, 0, 0.04)",
-                      },
-                    }}
-                    onClick={() =>
-                      !slot.disabled && handleSlotClick(slot.index)
-                    }
-                    disabled={slot.disabled}
-                  >
-                    {slot.time}
-                  </Button>
-                </Grid>
-              ))}
+              <Grid item xs={3}>
+                <Button
+                  variant="outlined"
+                  sx={{
+                    width: '100%',
+                    height: '100%',
+                    borderRadius: '4px',
+                    backgroundColor: selectedSlot && selectedSlot >= row[0].index && selectedSlot < row[row.length - 1].index ? 'green' : undefined,
+                    color: selectedSlot && selectedSlot >= row[0].index && selectedSlot < row[row.length - 1].index ? 'white' : undefined,
+                  }}
+                  onClick={() => handleSlotClick(row[0].index)}
+                  disabled
+                >
+                  {row[0].time}
+                </Button>
+              </Grid>
+              <Grid item xs={9}>
+                <Box sx={{ display: 'flex', flexDirection: 'column', height: `${row.length * 40}px`, overflow: 'hidden' }}>
+                  {row.map((slot) => (
+                    <Button
+                      key={slot.index}
+                      variant="outlined"
+                      sx={{
+                        width: '100%',
+                        height: '40px',
+                        borderRadius: '4px',
+                        backgroundColor: selectedSlot === slot.index ? 'green' : undefined,
+                        color: selectedSlot === slot.index ? 'white' : undefined,
+                      }}
+                      onClick={() => handleSlotClick(slot.index)}
+                    >
+                      {slot.time}
+                    </Button>
+                  ))}
+                </Box>
+              </Grid>
             </Grid>
           </Grid>
         ))}
