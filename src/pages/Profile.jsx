5c537c29e3da291fdc2019acc25f73fe19a00e72
mahmoudadelaziz <mahmoudaziz501@gmail.com>
2023-06-29 12:17:37 +0300

    IMPORTANT CHECKPOINT: the Profile page can now be wired up with the username. API Endpoints needed for linking to DB.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { useAuth } from "../AuthContext";
 
 export const Profile = () => {
+  const {    authUser,
+    SetAuthUser,
+    isLoggedIn,
+    SetIsLoggedIn,} = useAuth()
   return (
-    <h1 style={{textAlign: "center"}}>WELCOME TO YOUR PROFILE!</h1>  
+    <h1 style={{textAlign: "center"}}>WELCOME TO YOUR PROFILE, {authUser}!</h1>  
   );
 };
