48c04c2edf5012bf75722aa97b48564412521981
mahmoudadelaziz <mahmoudaziz501@gmail.com>
2023-06-29 11:02:59 +0300

    Set up a dummy Profile page, and wired up the NavDrawer in accordance with isAuth

--- src/components/NavDrawer.jsx
+++ src/components/NavDrawer.jsx
@@ -7,19 +7,7 @@ const NavDrawer = ({ drawerOpen, setDrawerOpen }) => {
   const NavItems = isLoggedIn
     ? [
         { title: "Home", icon: "home", route: "/" },
-        { title: "Login", icon: "login", route: "/signin" },
-        { title: "Signup", icon: "signup", route: "/signup" },
         { title: "Doctors", icon: "doctors", route: "/doctors" },
-        {
-          title: "Register your Practice",
-          icon: "doctors",
-          route: "/doctor/signup",
-        },
-        {
-          title: "Login to your Practice",
-          icon: "doctors",
-          route: "/doctor/login",
-        },
         { title: "Doctor's Profile", icon: "profile", route: "/profile" },
       ]
     : [
