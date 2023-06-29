1efe702553b58c9a449422f9ba39f04f1f41b4aa
mahmoudadelaziz <mahmoudaziz501@gmail.com>
2023-06-29 10:40:58 +0300

    Created the AuthContext custom hook.

---
+++ src/AuthContext.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useState, useContext } from "react";
+
+// Step 1: Creating the context
+const AuthContext = React.createContext();
+
+// Step 2: use the context (via a custom hook for simplicity)
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+// Step 3: Providing the context
+export function AuthProvider(props) {
+  const [authUser, SetAuthUser] = useState(null);
+  const [isLoggedIn, SetIsLoggedIn] = useState(false);
+
+  const value = {
+    authUser,
+    SetAuthUser,
+    isLoggedIn,
+    SetIsLoggedIn,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>{props.children}</AuthContext.Provider>
+  );
+}
